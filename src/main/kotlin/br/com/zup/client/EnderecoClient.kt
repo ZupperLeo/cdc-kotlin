fc2e719916a9f8a8524b0bae98b0d8a22b258364
ZupperLeo <leonardo.araujo@zup.com.br>
2021-09-22 14:08:04 -0300

    Trabalhando com formato XML e JSON

--- src/main/kotlin/br/com/zup/client/EnderecoClient.kt
+++ src/main/kotlin/br/com/zup/client/EnderecoClient.kt
@@ -2,12 +2,16 @@ package br.com.zup.client
 
 import br.com.zup.dto.EnderecoDTO
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 import io.micronaut.http.client.annotation.Client
 
 @Client("https://viacep.com.br/ws/")
 interface EnderecoClient {
 
-    @Get("{cep}/json")
-    fun consultarCEP(cep: String) : HttpResponse<EnderecoDTO>
+    @Get("{cep}/json/")//todo: verificar uma forma de mudar o formato que sera recebido atraves das variaveis de ambiente
+    @Consumes(MediaType.APPLICATION_XML)
+    fun consultarCEP(@QueryValue cep: String) : HttpResponse<EnderecoDTO>
 }
